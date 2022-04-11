84387bbf58cf92164d4739c47720500ca3894e99
Hal <72111003+devder@users.noreply.github.com>
2022-04-11 23:59:53 +0100

    add navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "../styles/globals.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-// const theme = createTheme({
-//   typography: {
-//     fontFamily: ["Poppins", "sans-serif"].join(","),
-//   },
-// });
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />;
-    </>
+    </ThemeProvider>
   );
 }
 
