1a61fa60a306f54054075199d64a57747ed47dfb
Hal <72111003+devder@users.noreply.github.com>
2022-04-11 22:06:26 +0100

    build layout component

---
+++ src/components/layout.tsx
@@ -0,0 +1,66 @@
+import { ReactNode, FC } from "react";
+import Head from "next/head";
+
+interface LayoutProps {
+  children: ReactNode;
+  description?: string;
+  author?: string;
+  title?: string;
+  meta?: Record<"name" | "content" | "property?", string>[];
+}
+
+export const Layout: FC<LayoutProps> = ({
+  children,
+  meta,
+  description = "This is the technical assessment for EukaPay Software Engineer Role",
+  author = "Ifeanyi Derick Iloabachie",
+  title = "Derick's Todo Management App",
+}) => {
+  const metaData = [
+    {
+      name: `description`,
+      content: description,
+    },
+    {
+      property: `og:title`,
+      content: title,
+    },
+    {
+      property: `og:description`,
+      content: description,
+    },
+    {
+      property: `og:type`,
+      content: `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: author,
+    },
+    {
+      name: `twitter:title`,
+      content: title,
+    },
+    {
+      name: `twitter:description`,
+      content: description,
+    },
+  ].concat(meta ?? []);
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        {metaData.map(({ name, content }) => (
+          <meta key={name} name={name} content={content} />
+        ))}
+      </Head>
+      <main role="main">{children}</main>
+    </>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { Layout } from "components/layout";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Testing </h1>
+    </Layout>
+  );
+};
+
+export default Home;
