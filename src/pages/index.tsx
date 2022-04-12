3232a670acbd82c6c19be862281835079ed916ec
Hal <72111003+devder@users.noreply.github.com>
2022-04-12 09:36:56 +0100

    create new-todo-form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import { Layout } from "components/layout";
+import { NewTodoForm } from "todos/components/new-todo-form";
 
 const Home: NextPage = () => {
   return (
     <Layout>
       <div className="container" style={{ margin: "auto" }}>
+        <NewTodoForm />
         Login to manage your todos
       </div>
     </Layout>
